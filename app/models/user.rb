894f9b319c305934acb3d925eb0c66b9cdae11c8
elise rochaix <elise.rochaix@gmail.com>
2023-05-29 17:03:35 +0200

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :vehicules, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :phone_number, presence: true
+
 end

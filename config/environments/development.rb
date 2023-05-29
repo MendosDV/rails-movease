a3282cb2d0fa311fafe83d353c5b9ab47f036f56
elise rochaix <elise.rochaix@gmail.com>
2023-05-29 16:22:45 +0200

    nouveau_form_sign_up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
 
 
+  config.active_storage.service = :cloudinary
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -38,7 +40,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  #config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false

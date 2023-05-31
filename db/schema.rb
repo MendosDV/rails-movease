0f44196f13042a68d65145ff4d3b8b3e22facb8f
Mickael Riss <mendos.prod@gmail.com>
2023-05-31 14:13:20 +0200

    Création de la carte

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_181136) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_085959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_181136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["category_id"], name: "index_vehicules_on_category_id"
     t.index ["user_id"], name: "index_vehicules_on_user_id"
   end

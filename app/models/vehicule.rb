512c8640f16f903ab619f0f0138d793dfddf9ec9
youcef haraghi <haraghiy@gmail.com>
2023-05-31 15:59:22 +0200

    fonctionnalité reserver

--- app/models/vehicule.rb
+++ app/models/vehicule.rb
@@ -2,8 +2,8 @@ class Vehicule < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_many :reservations
-  has_many_attached :pictures
+  has_many :reservations, dependent: :destroy
+  has_many_attached :pictures, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?

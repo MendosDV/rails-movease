b4c6e02eb36c33fd4eab48bf13b88076adfcbd2e
elise rochaix <elise.rochaix@gmail.com>
2023-05-31 15:45:15 +0200

    edit

--- app/models/vehicule.rb
+++ app/models/vehicule.rb
@@ -5,6 +5,10 @@ class Vehicule < ApplicationRecord
   has_many :reservations
   has_many_attached :pictures
 
+  validates :pictures, attached: true
+  validates :name, :description, :price, :category_id, :address, presence: true
+
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end

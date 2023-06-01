6ff189bee496cb59ddec2712ee77544afbd6beaa
elise rochaix <elise.rochaix@gmail.com>
2023-06-01 08:37:53 +0200

    chgnt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  before_save :set_default_avatar
+
   # Include default devise modules. Others available are:
+
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :reservations
   has_many :vehicules, dependent: :destroy
@@ -10,9 +13,14 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
- 
+
   validates :first_name, :last_name, :email, :password, :phone_number, presence: true
   validates :email, uniqueness: true
 
+
+
+  def set_default_avatar
+    self.avatar ||= 'app/assets/images/users/avatar_default.png'
+  end
 
 end
